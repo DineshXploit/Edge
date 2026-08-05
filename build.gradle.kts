f3fe5b1d68935c154acd75b1c1871804dafcd31b
DineshXploit <dineshyaduvansi258@gmail.com>
2026-08-05 05:33:14 +0530

    Resolve Android build conflict versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
+    id("com.android.application") version "8.11.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10" apply false
     id("com.google.dagger.hilt.android") version "2.57" apply false
-    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
+    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
 }
--- gradlew
+++ gradlew
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 ROOT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-if [ -x "$ROOT_DIR/gradle/wrapper/gradle-wrapper.jar" ]; then
+if [ -f "$ROOT_DIR/gradle/wrapper/gradle-wrapper.jar" ]; then
   exec java -jar "$ROOT_DIR/gradle/wrapper/gradle-wrapper.jar" "$@"
 fi
 if [ -d /root/.local/share/mise/installs/java/21.0.2 ]; then
